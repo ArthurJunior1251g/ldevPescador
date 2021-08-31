8e556229001b0a148f8625125bed02e737622df2
Felipe Roberti Amaral <77501866+flproberti@users.noreply.github.com>
2021-08-31 15:01:34 -0300

    Add files via upload

---
+++ ldevConfig.lua
@@ -0,0 +1,34 @@
+ldevPescador = {
+    
+    ['Notificações'] = {
+        ['SemIsca'] = {'Você precisa de ',' minhocas para pescar'}
+    },
+    
+    ['Pescaria'] = {
+        ['Veículos'] = {
+            1033245328,
+            276773164,
+            509498602,
+            867467158,
+            861409633,
+            -1043459709,
+            231083307,
+            437538602,
+            400514754,
+            -282946103,
+            1070967343,
+            908897389,
+            290013743,
+            1448677353,
+            -2100640717,
+            -980573366,
+            1861786828,
+            -276744698,
+        },
+        ['TempoProgresso'] = 60,
+        ['ItemVaraDePesca'] = 'varadepesca',
+        ['ItemIsca'] = 'minhoca',
+        ['QtdItemIsca'] = 1,
+        ['ItensPeixes'] = { 'anchova','atum','barracuda','bicuda','corvina','pampo','tainha' },
+    }, 
+}
