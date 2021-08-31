00b561e4d22575c44425e195ecf025fb9e6b97b2
Felipe Roberti Amaral <77501866+flproberti@users.noreply.github.com>
2021-08-31 15:02:57 -0300

    Add files via upload

---
+++ ldevPescador/ldevServer.lua
@@ -0,0 +1,45 @@
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--( Tunnel )-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+local Tunnel = module('vrp','lib/Tunnel')
+local Proxy = module('vrp','lib/Proxy')
+vRP = Proxy.getInterface('vRP')
+scriptPescador = {}
+Tunnel.bindInterface('ldevPescador',scriptPescador)
+pescadorCL = Tunnel.getInterface('ldevPescador')
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--( Funções )------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function scriptPescador.verificaItens() 
+    local source = source
+    local user_id = vRP.getUserId(source)
+    if user_id then
+        local quantidadeNecessaria = ldevPescador['Pescaria']['QtdItemIsca']
+        local quantidadePlayer = vRP.getInventoryItemAmount(user_id,ldevPescador['Pescaria']['ItemIsca'])
+        if vRP.hasItem(user_id,ldevPescador['Pescaria']['ItemVaraDePesca'],1) then
+            if vRP.hasItem(user_id,ldevPescador['Pescaria']['ItemIsca'],quantidadeNecessaria) then
+                return true
+            else 
+                TriggerClientEvent('Notify',source,'aviso',ldevPescador['Notificações']['SemIsca'][1] .. quantidadeNecessaria-quantidadePlayer .. 'x' .. ldevPescador['Notificações']['SemIsca'][2])
+                return false
+            end
+        end
+    end
+end
+
+function scriptPescador.verificaRecompensa()
+    local source = source
+    local user_id = vRP.getUserId(source)
+    if user_id then
+        local randompeixe = math.random(#ldevPescador['Pescaria']['ItensPeixes'])
+        if vRP.getInventoryWeight(user_id)+vRP.getItemWeight(ldevPescador['Pescaria']['ItensPeixes'][randompeixe])*1 <= vRP.getInventoryMaxWeight(user_id) then
+            if vRP.tryGetInventoryItem(user_id,ldevPescador['Pescaria']['ItemIsca'],ldevPescador['Pescaria']['QtdItemIsca'],false) then
+                vRP.giveInventoryItem(user_id,ldevPescador['Pescaria']['ItensPeixes'][randompeixe],1)
+                return true
+            end
+        else
+            TriggerClientEvent('Notify',source,'negado',ldevLeiteiro['Notificações']['MochilaCheia'])
+            return false
+        end
+    end
+end
