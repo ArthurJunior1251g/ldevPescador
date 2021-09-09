61243f9837bbc4d12baad30fb5593f83b8b31920
Felipe Roberti Amaral <77501866+flproberti@users.noreply.github.com>
2021-09-09 00:16:04 -0300

    Update ldevServer.lua

--- ldevPescador/ldevServer.lua
+++ ldevPescador/ldevServer.lua
@@ -7,6 +7,7 @@ vRP = Proxy.getInterface('vRP')
 scriptPescador = {}
 Tunnel.bindInterface('ldevPescador',scriptPescador)
 pescadorCL = Tunnel.getInterface('ldevPescador')
+local randompeixe
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --( Funções )------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -16,13 +17,16 @@ function scriptPescador.verificaItens()
     if user_id then
         local quantidadeNecessaria = ldevPescador['Pescaria']['QtdItemIsca']
         local quantidadePlayer = vRP.getInventoryItemAmount(user_id,ldevPescador['Pescaria']['ItemIsca'])
-        if vRP.hasItem(user_id,ldevPescador['Pescaria']['ItemVaraDePesca'],1) then
-            if vRP.hasItem(user_id,ldevPescador['Pescaria']['ItemIsca'],quantidadeNecessaria) then
+        if vRP.getInventoryItemAmount(user_id,ldevPescador['Pescaria']['ItemVaraDePesca']) >= 1 then
+            if vRP.getInventoryItemAmount(user_id,ldevPescador['Pescaria']['ItemIsca']) >= quantidadeNecessaria then
                 return true
             else 
                 TriggerClientEvent('Notify',source,'aviso',ldevPescador['Notificações']['SemIsca'][1] .. quantidadeNecessaria-quantidadePlayer .. 'x' .. ldevPescador['Notificações']['SemIsca'][2])
                 return false
             end
+        else 
+            TriggerClientEvent('Notify',source,'negado',ldevPescador['Notificações']['SemVara'])
+            return false
         end
     end
 end
@@ -31,7 +35,7 @@ function scriptPescador.verificaRecompensa()
     local source = source
     local user_id = vRP.getUserId(source)
     if user_id then
-        local randompeixe = math.random(#ldevPescador['Pescaria']['ItensPeixes'])
+        randompeixe = math.random(#ldevPescador['Pescaria']['ItensPeixes'])
         if vRP.getInventoryWeight(user_id)+vRP.getItemWeight(ldevPescador['Pescaria']['ItensPeixes'][randompeixe])*1 <= vRP.getInventoryMaxWeight(user_id) then
             if vRP.tryGetInventoryItem(user_id,ldevPescador['Pescaria']['ItemIsca'],ldevPescador['Pescaria']['QtdItemIsca'],false) then
                 return true
@@ -49,5 +53,6 @@ function scriptPescador.recebeRecompensa()
     if user_id then  
         Wait(ldevPescador['Pescaria']['TempoProgresso']*1000)
         vRP.giveInventoryItem(user_id,ldevPescador['Pescaria']['ItensPeixes'][randompeixe],1) 
+        randompeixe = nil
     end
 end
